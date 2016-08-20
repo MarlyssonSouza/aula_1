e0894a9727e61993321ce077b6ccce35771c27a4
Marlysson <marlysson19@gmail.com>
2016-08-20 17:56:43 -0300

    fim de aula

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-Hello World!
+<?php
+
+$variavel="Hello Word!";
+
+$soma=0+2;
+
+$mod=10%2;
+
+?>
+<!DOCTYPE html>
+<html>
+  <?php
+    include "layout/head.php";
+
+    require_once "layout/body.php";
+   ?>
+
+</html>
